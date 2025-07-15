39458db8e59a015efd2168b6814aca4c115fbdc9
Alexey Kohan <kohanzom@gmail.com>
2025-07-15 18:30:22 +0300

    Create dialog_state.py

---
+++ NeuroTalk/bot/services/dialog_state.py
@@ -0,0 +1,8 @@
+dialog_history = {}
+
+def add_to_history(user_id: int, role: str, content: str):
+    if user_id not in dialog_history:
+        dialog_history[user_id] = []
+    dialog_history[user_id].append({"role": role, "content": content})
+    if len(dialog_history[user_id]) > 15:
+        dialog_history[user_id] = dialog_history[user_id][-15:]
