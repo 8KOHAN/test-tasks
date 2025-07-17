51adf3744b4a991c80a5de3c468ee433a6b2b49c
Alexey Kohan <kohanzom@gmail.com>
2025-07-17 17:57:01 +0300

    Update main.py

--- NeuroTalk/bot/main.py
+++ NeuroTalk/bot/main.py
@@ -1,18 +1,31 @@
+import asyncio
+import os
+
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 
+from handlers import base, booking, start
+
+load_dotenv()
+
 async def main():
-    
-    load_dotenv()
     token = os.getenv("BOT_TOKEN")
-    bot = Bot(token)
+    if not token:
+        raise ValueError("BOT_TOKEN не заданий у .env")
+
+    bot = Bot(token=token)
     dp = Dispatcher()
 
-    print("bot start")
+    dp.include_router(start.router)
+    dp.include_router(booking.router)
+    dp.include_router(base.router)
+
+    print("🤖 Бот запущено!")
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print("exit")
+        print("🛑 Зупинка бота")
