b3d2ceeef52b03f329cce74710e64f007c99fab5
Geralt Roger <kohanzom@gmail.com>
2025-10-13 17:09:02 +0300

    Update booking.py

--- NeuroTalk/bot/handlers/booking.py
+++ NeuroTalk/bot/handlers/booking.py
@@ -7,9 +7,7 @@ router = Router()
 @router.message(Command("booking"))
 async def handle_booking_button(message: types.Message):
     await message.answer(
-        "Щоб забронювати час, перейдіть за посиланням нижче:\n"
-        f"[🔗 Перейти в Google Календар]({GOOGLE_CALENDAR_HTTPS})",
+        "To book an appointment, follow the link below:\n"
+        f"[🔗 Go to Google Calendar]({GOOGLE_CALENDAR_HTTPS})",
         parse_mode="Markdown"
     )
-
-
