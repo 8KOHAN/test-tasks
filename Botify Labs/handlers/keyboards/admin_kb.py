1c9252c8ed25e863105b5bda8836ffd70c078f03
Geralt Roger <kohanzom@gmail.com>
2025-10-13 17:02:38 +0300

    Update admin_kb.py

--- Botify Labs/handlers/keyboards/admin_kb.py
+++ Botify Labs/handlers/keyboards/admin_kb.py
@@ -2,22 +2,22 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 def admin_main_kb():
     return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="➕ Добавить товар", callback_data="admin_add")],
-        [InlineKeyboardButton(text="📂 Категории", callback_data="admin_cat")]
+        [InlineKeyboardButton(text="➕ Add product", callback_data="admin_add")],
+        [InlineKeyboardButton(text="📂 Categories", callback_data="admin_cat")]
     ])
 
 def admin_category_kb():
     return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="🖥 Компьютеры", callback_data="admin_cat_manage_computers")],
-        [InlineKeyboardButton(text="📱 Телефоны", callback_data="admin_cat_manage_phones")],
-        [InlineKeyboardButton(text="🔙 Назад", callback_data="back_admin_main")]
+        [InlineKeyboardButton(text="🖥 Computers", callback_data="admin_cat_manage_computers")],
+        [InlineKeyboardButton(text="📱 Phones", callback_data="admin_cat_manage_phones")],
+        [InlineKeyboardButton(text="🔙 Back", callback_data="back_admin_main")]
     ])
 
 def add_category_kb():
     return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="🖥 Компьютеры", callback_data="admin_add_cat_computers")],
-        [InlineKeyboardButton(text="📱 Телефоны", callback_data="admin_add_cat_phones")],
-        [InlineKeyboardButton(text="🔙 Назад", callback_data="back_admin_main")]
+        [InlineKeyboardButton(text="🖥 Computers", callback_data="admin_add_cat_computers")],
+        [InlineKeyboardButton(text="📱 Phones", callback_data="admin_add_cat_phones")],
+        [InlineKeyboardButton(text="🔙 Back", callback_data="back_admin_main")]
     ])
 
 def product_list_kb(category: str, products: dict):
@@ -31,6 +31,6 @@ def product_list_kb(category: str, products: dict):
                 )
             ])
     buttons.append([
-        InlineKeyboardButton(text="◀️ Назад", callback_data="back_admin_categories")
+        InlineKeyboardButton(text="◀️ Back", callback_data="back_admin_categories")
     ])
-    return InlineKeyboardMarkup(inline_keyboard=buttons or [[InlineKeyboardButton(text="(пусто)", callback_data="none")]])
+    return InlineKeyboardMarkup(inline_keyboard=buttons or [[InlineKeyboardButton(text="(empty)", callback_data="none")]])
